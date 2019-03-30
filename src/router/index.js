2ae3d9906bb125610150028d30b08c4053b7518a
luoxiapeng <949123073@qq.com>
2019-03-30 23:52:55 +0800

    去除hash搭建路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,12 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
+    {
+      path: '/',
+      redirect: '/HelloWorld'
+    },
     {
       path: '/',
       name: 'HelloWorld',

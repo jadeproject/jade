15caecc1587e9254a9b4669452c63db33eb5a525
luoxiapeng <949123073@qq.com>
2019-03-31 01:26:08 +0800

    封装请求，以及处理公共类库

--- src/main.js
+++ src/main.js
@@ -3,15 +3,36 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/css/base.css'
+
 
 // 引入iview框架
 import iView from 'iview'; // 导入组件库
 import 'iview/dist/styles/iview.css'; // 导入样式
 Vue.use(iView);
 
-Vue.config.productionTip = false
+// 个人的包
+import './assets/css/base.css'
+// 请求封装
+import {post,get,patch,put} from '../common/require'
+//定义全局变量
+Vue.prototype.$post=post;
+Vue.prototype.$get=get;
+Vue.prototype.$patch=patch;
+Vue.prototype.$put=put;
 
+// 请求示例:
+/**
+ *
+ * @type {boolean}
+ * 第一个参数为：api地址
+ * 第二个参数为：{}请求的入参
+ */
+// this.$get('/api/v2/movie/top250',{})
+//     .then((response) => {
+//         console.log(response)
+//     })
+
+Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',

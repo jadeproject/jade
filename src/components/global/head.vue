abefffd9c9dc750047cb8568bf4b59c362175203
luoxiapeng <949123073@qq.com>
2019-03-31 14:48:43 +0800

    登陆注册逻辑弹窗已完成

--- src/components/global/head.vue
+++ src/components/global/head.vue
@@ -13,18 +13,23 @@
           <div>产品介绍</div>
           <div>开奖公告</div>
           <div>常见问题</div>
-          <div><span class="iconfont">&#xe602;</span></div>
+          <div @click="longinShow()"><span class="iconfont">&#xe602;</span></div>
         </div>
       </div>
-      <login></login>
+      <login ref="loginT" @On_click="log_click"></login>
+      <login-pop ref="log_Pop"></login-pop>
+
     </div>
 
 </template>
 <script type="text/ecmascript-6">
   import login from '@/components/login/login'
+  import loginPop from '@/components/login/loginPop'
     export default {
         data() {
-            return {};
+            return {
+              showLogin:true
+            };
         },
         created() {
 
@@ -32,9 +37,33 @@
         mounted() {
 
         },
-        methods: {},
+        methods: {
+          // 控制右边切换
+          longinShow(){
+            if(this.showLogin==true){
+              this.$refs.loginT.hiden();
+              this.showLogin=false;
+              this.$refs.log_Pop.hiden();
+            }else {
+              this.$refs.loginT.show();
+              this.showLogin=true;
+              this.$refs.log_Pop.hiden();
+            }
+
+          },
+          // 点击登陆注册显示不同的窗体
+          log_click(data){
+           if(data.type=='login'){
+             this.$refs.log_Pop.showlogin();
+           }
+           if(data.type=='reg'){
+             this.$refs.log_Pop.showreg();
+           }
+          }
+        },
         components: {
-          login
+          login,
+          loginPop
         }
     };
 </script>
@@ -42,12 +71,18 @@
 <style type="text/css" lang="less">
   .headC{
     /*height: 20px;*/
+    height: 40px;
     width: 100%;
+    position: fixed;
+    left: 0;
+    top: 0;
+    background-color: #fff;
+    
     .headC_key{
       width: 100%;
-      height: 40px;
       line-height: 40px;
       display: flex;
+      cursor:pointer;
       .left{
         width: 30%;
         height: 100%;
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,18 +1,42 @@
 <template>
-    <div class="loginC">
+    <div class="loginC" v-if="flag">
       <div class="noLogin" v-if="type==1">
-         <div>
+         <div @click="loginPonClick()">
            <span class="iconfont">&#xe61f;</span>
            <span  class="name">登陆</span>
          </div>
-         <div>
+         <div @click="regPonClick()">
            <span class="iconfont">&#xe613;</span>
            <span class="name">注册</span>
          </div>
       </div>
-      <div class="loginInfo">
+      <div class="loginInfo" v-if="type==2">
         <div class="info">
+          <img src="../../assets/img/m_avatar.png" alt="">
           <span class="name">王小明</span>
+        </div>
+        <div>
+           <span class="iconfont">&#xe61f;</span>
+           <span class="name">玉石中心</span>
+        </div>
+        <div>
+          <span class="iconfont">&#xe652;</span>
+          <span class="name">订单中心</span>
+        </div>
+        <div>
+          <span class="iconfont">&#xe630;</span>
+          <span class="name">活动中心</span>
+        </div>
+        <div>
+          <span class="iconfont">&#xe655;</span>
+          <span class="name">账户中心</span>
+        </div>
+        <div>
+          <span class="iconfont">&#xe604;</span>
+          <span class="name">邀请好友</span>
+        </div>
+        <div class="exit">
+          退出登陆
         </div>
 
       </div>
@@ -22,12 +46,20 @@
 </template>
 
 <script type="text/ecmascript-6">
-   import imgurl from '../../assets/img/m_avatar.png'
     export default {
         data() {
             return {
-              type:2,
-              imgurl:'../../assets/img/m_avatar.png'
+              // 对应不同类型
+              type:1,
+              // 控制显示隐藏
+              flag:false,
+              loginkey:{
+                type:'login'
+              },
+              regKey:{
+                type:'reg'
+              }
+
             };
         },
         created() {
@@ -35,8 +67,22 @@
         },
         mounted() {
 
+
         },
-        methods: {},
+        methods: {
+          show(){
+             this.flag=true;
+          },
+          hiden(){
+            this.flag=false;
+          },
+          loginPonClick(){
+            this.$emit('On_click',this.loginkey)
+          },
+          regPonClick(){
+            this.$emit('On_click',this.regKey)
+          }
+        },
         components: {}
     };
 </script>
@@ -45,15 +91,14 @@
   @import "../../assets/css/config";
   .loginC{
     width: 10%;
-    padding:10px;
     background-color: #9e9fa1;
-    z-index: 1;
+    z-index: 10000000000;
     position: fixed;
     right: 0;
     top: 40px;
     .noLogin,.loginInfo{
       div{
-        padding: 20px 0;
+        padding: 25px 0;
         text-align: center;
         font-size: 14px;
         cursor:pointer;
@@ -67,13 +112,29 @@
         }
 
       }
+      div{
+        padding: 12px 0;
+      }
       .info{
-        padding: 30px 0;
+        padding:20px 0;
+        .border-1px(#000);
+        img{
+          height: 30px;
+          width: 30px;
+          border-radius: 50%;
+        }
         .name{
           color: #fff;
           margin-left: 10px;
         }
 
+
+      }
+      .exit{
+        line-height: 30px;
+        text-align: center;
+        background-color: @bg;
+        color: #fff;
       }
 
 
